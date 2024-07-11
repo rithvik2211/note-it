9e43c1f3fc4574659eca6a81c02205a7cbc4d20a
Sandadi Rithvik Reddy <srreddy@EX2INDIA.LOCAL>
2024-07-11 16:42:25 +0530

    nested document creation complted

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,20 +1,29 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronLeft, MenuIcon } from "lucide-react";
+import {
+  ChevronLeft,
+  MenuIcon,
+  PlusCircle,
+  Search,
+  Settings2,
+} from "lucide-react";
 import { clearModuleContext } from "next/dist/server/lib/render-server";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
+import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
 
-  const documents = useQuery(api.documents.get);
+  const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -103,6 +112,16 @@ const Navigation = () => {
       }, 300);
     }
   };
+
+  const handleCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new node...",
+      success: "New note Created!",
+      error: "Failed to create a New note.",
+    });
+  };
   return (
     <>
       <aside
@@ -125,11 +144,12 @@ const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item label="Settings" icon={Settings2} onClick={() => {}} />
+          <Item label="Search" icon={Search} isSearch onClick={() => {}} />
+          <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
 
         <div
