9af8571c8c6e1abb3a2f27bbc457ae054e728a55
Sandadi Rithvik Reddy <srreddy@EX2INDIA.LOCAL>
2024-07-15 12:06:08 +0530

    code push

--- convex/documents.ts
+++ convex/documents.ts
@@ -5,6 +5,45 @@ import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 import { title } from "process";
 
+export const archive = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+    const userId = identity.subject;
+
+    const exsistingDocument = await ctx.db.get(args.id);
+
+    if (!exsistingDocument) throw new Error("Not found.");
+
+    if (exsistingDocument.userId !== userId) {
+      throw new Error("Unauthorised");
+    }
+
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", documentId)
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: true,
+        });
+        await recursiveArchive(child._id);
+      }
+    };
+
+    const document = await ctx.db.patch(args.id, {
+      isArchived: true,
+    });
+  },
+});
+
 export const create = mutation({
   args: {
     title: v.string(),
